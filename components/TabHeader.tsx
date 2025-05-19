1d8bc287f15cd7e51a8f19ed9c5c0bf4756388ff
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2025-05-19 18:32:32 +0700

    Update TabHeader.tsx

--- components/TabHeader.tsx
+++ components/TabHeader.tsx
@@ -7,8 +7,13 @@ export function TabHeader({ title }: { title: string }) {
   const router = useRouter();
   return (
     <View className="flex-row items-center p-4 border-b border-gray-200">
-      <TouchableOpacity onPress={() => router.back()} className="mr-4">
-        <Ionicons name="arrow-back" size={24} color="#265B3F" />
+      <TouchableOpacity
+        onPress={() => {
+          router.back();
+        }}
+        className="mr-4"
+      >
+        <Ionicons name="chevron-back" size={24} color="#265B3F" />
       </TouchableOpacity>
       <Text className="text-xl font-bold">{title}</Text>
     </View>
