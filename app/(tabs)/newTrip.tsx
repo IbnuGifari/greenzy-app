0fe35287c8815e4916ca4149bdfd2698b0aaad5c
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2025-05-30 13:26:50 +0700

    Update newTrip.tsx

--- app/(tabs)/newTrip.tsx
+++ app/(tabs)/newTrip.tsx
@@ -1,17 +1,13 @@
 import { TabHeaderTransparent } from "@/components/TabHeaderTransparent";
 import React from "react";
-import { ScrollView, Text } from "react-native"; // Import necessary components from react-native
+import { ScrollView } from "react-native"; // Import necessary components from react-native
 import { SafeAreaView } from "react-native-safe-area-context"; // Import SafeAreaView for safe area handling
 
 export default function NewTripScreen() {
   return (
     <SafeAreaView className="flex-1 bg-[#F6F6F9]">
       <TabHeaderTransparent title="Perjalanan Baru" />
-      <ScrollView className="flex-1 px-4">
-        <Text className="text-lg font-semibold mb-4">
-          Pilih Jenis Perjalanan
-        </Text>
-      </ScrollView>
+      <ScrollView className="flex-1 px-4"></ScrollView>
     </SafeAreaView>
   );
 }
