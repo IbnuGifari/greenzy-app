41375e6dde1cdd2cc527835b3dd39245caccf8ad
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2025-05-30 13:26:37 +0700

    Update tripHistory.tsx

--- app/(tabs)/tripHistory.tsx
+++ app/(tabs)/tripHistory.tsx
@@ -1,7 +1,9 @@
 import { FootPrintsGraph } from "@/components/FootprintsGraph";
 import { HistoryList } from "@/components/HistoryList"; // Your existing component
 import { TabHeader } from "@/components/TabHeader";
+import { TripOverviewCard } from "@/components/TripOverviewCard";
 import { travelHistory } from "@/data/travelData"; // Your existing data
+import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import React from "react";
 import { ScrollView, Text, View } from "react-native";
@@ -14,6 +16,35 @@ export default function TripHistoryScreen() {
     <SafeAreaView className="flex-1 bg-[#F6F6F9]">
       <TabHeader title="Riwayat Perjalanan" />
       <FootPrintsGraph />
+      <View className="p-4">
+        <Text className="text-lg font-semibold mb-4">Statistik Perjalanan</Text>
+        <View className="flex-row flex-wrap justify-between gap-4">
+          <TripOverviewCard
+            title="Total Perjalanan"
+            value={travelHistory.length}
+            units="Perjalanan"
+            icon={<Ionicons name="car" size={32} color="#2E4D30" />}
+          />
+          <TripOverviewCard
+            title="Total Emisi"
+            value={69} // {travelHistory.reduce((acc, trip) => acc + trip.carbonEmissions, 0)}
+            units="kg CO2"
+            icon={<Ionicons name="leaf" size={32} color="#2E4D30" />}
+          />
+          <TripOverviewCard
+            title="Total Jarak Perjalanan"
+            value={189} // {travelHistory.reduce((acc, trip) => acc + trip.carbonEmissions, 0) / travelHistory.length}
+            units="km"
+            icon={<Ionicons name="stats-chart" size={32} color="#2E4D30" />}
+          />
+          <TripOverviewCard
+            title="Rata-rata Jarak Perjalanan"
+            value={5.9}
+            units="km"
+            icon={<Ionicons name="analytics" size={32} color="#2E4D30" />}
+          />
+        </View>
+      </View>
       <View>
         <Text className="text-lg font-semibold mb-4 px-4">Riwayat</Text>
       </View>
